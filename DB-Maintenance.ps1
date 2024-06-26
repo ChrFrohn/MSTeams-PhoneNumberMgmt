7f8eb052393af0632d24e7efe5f3ee6929571317
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-06-26 14:31:06 +0200

    Update DB-Maintenance.ps1
    fixed trim

--- DB-Maintenance.ps1
+++ DB-Maintenance.ps1
@@ -44,52 +44,50 @@ $teamstokenBody = @{
 $teamsToken = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token" -Method POST -Body $teamstokenBody | Select-Object -ExpandProperty Access_Token 
 Connect-MicrosoftTeams -AccessTokens @("$graphToken", "$teamsToken")
 
-
-
 # Retrieve Teams users and DB users
 $TeamsUsers = Get-CsOnlineUser | Select-Object Alias, LineURI
 $Query_UsersInDB = "SELECT * FROM $DBTableName1 WHERE UsedBy IS NOT NULL;"
 $UsersInDB = Invoke-Sqlcmd -ServerInstance $SQLServer -Database $DBName -AccessToken $AccessToken -Query $Query_UsersInDB -Verbose
-
+ 
 # Function to update DB with Teams user information if not already present
 function Update-DatabaseWithTeamsUserInfo {
     param (
         [Parameter(Mandatory = $true)]
         $User
     )
-
+ 
     $userInDB = $UsersInDB | Where-Object { $_.UsedBy -eq $User.Alias }
-
+ 
     if (-not $userInDB -and -not [string]::IsNullOrWhiteSpace($User.LineURI)) {
-        $UserPhoneNumber = $User.LineURI.TrimStart('TEL:+45')
+        $UserPhoneNumber = $User.LineURI.Substring(7)
         $UserNameInTeams = $User.Alias
         Write-Host "DB Update for $UserNameInTeams $UserPhoneNumber"
         $Query_UsersInDB_Add = "UPDATE $DBTableName1 SET UsedBy='$UserNameInTeams' WHERE PSTNnumber ='$UserPhoneNumber'"
         Invoke-Sqlcmd -ServerInstance $SQLServer -Database $DBName -AccessToken $AccessToken -Query $Query_UsersInDB_Add -Verbose
     }
 }
-
+ 
 # Function to release number in DB if user not found in Teams
 function Release-DBNumberForNonTeamsUser {
     param (
         [Parameter(Mandatory = $true)]
         $DBUser
     )
-
+ 
     $userInTeams = $TeamsUsers | Where-Object { $_.Alias -eq $DBUser }
-
+ 
     if (-not $userInTeams) {
         Write-Host "$DBUser is not found in Microsoft Teams, user will be removed from the DB and number will become available"
         $Query_UsersInDB_CleanUp = "UPDATE $DBTableName1 SET UsedBy=NULL WHERE UsedBy ='$DBUser'"
         Invoke-Sqlcmd -ServerInstance $SQLServer -Database $DBName -AccessToken $AccessToken -Query $Query_UsersInDB_CleanUp -Verbose
     }
 }
-
+ 
 # Update DB with Teams users info
 foreach ($User in $TeamsUsers) {
     Update-DatabaseWithTeamsUserInfo -User $User
 }
-
+ 
 # Release numbers for users not in Teams
 foreach ($DBUser in $UsersInDB.UsedBy) {
     Release-DBNumberForNonTeamsUser -DBUser $DBUser
